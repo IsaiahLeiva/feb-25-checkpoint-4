5e96cdd62f80c41a3d307ca53d64574b4c58d06f
Isaiah <isaiah.leiva@gmail.com>
2022-02-25 16:51:09 -0700

    commit

--- app/Services/AxiosService.js
+++ app/Services/AxiosService.js
@@ -1,10 +1,26 @@
+export const sandBoxApi = axios.create({
+    baseURL: "https://bcw-sandbox.herokuapp.com/api/IsaiahL/quotes",
+    timeout: 5000
+});
+
 export const quoteApi = axios.create({
     baseURL: 'https://bcw-sandbox.herokuapp.com/api/quotes',
     timeout: 5000
 })
 
 
-export const sandBoxApi = axios.create({
-    baseURL: "https://bcw-sandbox.herokuapp.com/api/IsaiahL/quotes",
+export const weatherApi = axios.create({
+    baseURL: 'https://bcw-sandbox.herokuapp.com/api/weather',
     timeout: 5000
-});
+})
+
+export const imagesApi = axios.create({
+    baseURL: 'https://bcw-sandbox.herokuapp.com/api/images',
+    timeout: 5000
+})
+
+export const todosApi = axios.create({
+    baseURL: 'https://bcw-sandbox.herokuapp.com/api/ISAIAH/todos',
+    timeout: 5000
+})
+
