ccf9c5425a2e340d630a7bd5a11d238ba0337617
Isaiah <isaiah.leiva@gmail.com>
2022-02-25 13:58:36 -0700

    commit

--- app/Models/Quote.js
+++ app/Models/Quote.js
@@ -1,18 +1,20 @@
+import { generateId } from "../Utils/generateId";
 
 
 export default class Quote {
     constructor(data) {
+        this.id = generateId()
         this.character = data.character || data.character;
         this.quote = data.quote || data.quote
     }
 
     get ActiveQuote() {
         return `
-        <div class="text-center">
-        <h3>"${this.quote}"</h3>
+        <div id="quotes" class="text-center">
+        <h3 class="fst-italic">"${this.quote}"</h3>
+        //TODO: GET THIS TO OVERLAY
         <h4>-${this.character}</h4>
         </div>
         `
     }
-
 }
