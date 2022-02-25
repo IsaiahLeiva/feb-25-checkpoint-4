7b17b9dfd79399eb14ea2bc26bea6e93351c5fea
Isaiah <isaiah.leiva@gmail.com>
2022-02-25 15:58:55 -0700

    commit

---
+++ app/Controllers/TasksController.js
@@ -0,0 +1,27 @@
+
+
+
+export class TasksController {
+    createTask(TaskID, event) {
+        console.log('creating a task');
+        console.log(event.target)
+        window.event.preventDefault()
+
+        const form = window.event.target
+
+        const newTask = {
+            TaskID,
+            name: form.taskname.value
+        }
+        console.log(event.target.parentNode)
+        event.target.parentNode.innerHTML += `<div class = 'testing'></div>`
+        console.log("[TaskController]: createTask", newTask)
+        tasksService.createTask(newTask)
+    }
+
+    async deleteTask(id) {
+        if (await Pop.confirm()) {
+            tasksService.deleteTask(id)
+        }
+    }
+}
--- app/Models/Task.js
+++ app/Models/Task.js
@@ -1,16 +1,22 @@
 
 
-export class TodoTask {
+
+
+export class Task {
     constructor(data) {
-        this.id = data.id || generateID()
+        this.id = data.id || generateId()
         this.name = data.name
         this.checkbox = data.checkbox || true
+        this.listId = data.listId
+
     }
 
     get Template() {
         return `
         <input type="checkbox" name="list item checkbox" id=""><li class="d-flex justify-content-between">${this.name} <i class="mdi mdi-delete selectable" title="Delete ${this.name}" onclick="app.tasksController.deleteTask('${this.id}')"></i></li>
         `
+
+
     }
 
 }
