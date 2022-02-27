c77b784df69150b799f5149fcf4b2e4abebdd7e9
Isaiah <isaiah.leiva@gmail.com>
2022-02-26 19:13:58 -0700

    commit

--- app/Models/weather.js
+++ app/Models/weather.js
@@ -9,7 +9,21 @@ export class weather {
     get Template() {
         return `
         <h1 class="">${this.temperature}</h1>
+        <li>${this.Fahrenheit}</li>
+        <li>${this.Celsius}</li>
         `
+    }
+
+    get Fahrenheit() {
+        let F = 0
+        F = ((this.temperature - 273.15) * 1.8) + 32
+        return F.toFixed(0)
+    }
+
+    get Celsius() {
+        let C = 0
+        C = (this.temperature - 273.15)
+        return C.toFixed(0)
     }
 }
 
@@ -19,4 +33,6 @@ export class weather {
 //NOTE: Kelvin to Celsius
 // ℃=K-273.15	
 
+//NOTE: ternary operator: 
+condition ? iftrue : iffalse
 
