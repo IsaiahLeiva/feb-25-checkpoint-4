d9265313e7b5f10a14cebdc594b4aac3fbcc91cb
Isaiah <isaiah.leiva@gmail.com>
2022-02-28 10:44:34 -0700

    commit

--- app/Services/QuotesService.js
+++ app/Services/QuotesService.js
@@ -5,6 +5,7 @@ import { quoteApi } from "./AxiosService.js";
 export class QuotesService {
     async addQuote() {
         const res = await quoteApi.get()
+        //NOTE: Go over line 9 and how it works
         console.log(res.data)
         ProxyState.quotes = new Quote(res.data)
         ProxyState.quotes = ProxyState.quotes
